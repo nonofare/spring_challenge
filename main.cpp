7dda6d7926b3f9277479b39953c3fa7a77499a3b
Adam Wojturski <adik96885@gmail.com>
2025-04-06 17:42:49 +0200

    Running build

--- main.cpp
+++ main.cpp
@@ -2,210 +2,243 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <cstdint>
+#include <bitset>
 
 using namespace std;
-#define DIMENSION 3
 
-int **get_board(int **copy_board = nullptr) {
-    int **board = new int *[DIMENSION];
-    for (int i = 0; i < DIMENSION; i++) {
-        board[i] = new int[DIMENSION];
-    }
+// reprezentuje maksymalna wysokosc i szerokosc planszy
+#define DIM 3
 
-    if (copy_board) {
-        for (int i = 0; i < DIMENSION; i++) {
-            for (int j = 0; j < DIMENSION; j++) {
-                board[i][j] = copy_board[i][j];
-            }
+class Board {
+    // reprezentuje plansze 2d
+    int **field;
+
+public:
+    // konstruktor uzywany tylko raz w main()
+    Board() {
+        field = new int *[DIM];
+        for (int i = 0; i < DIM; i++) {
+            field[i] = new int[DIM]();
         }
     }
 
-    return board;
-}
+    // konstruktor uzywany w kazdej rekurencji simulate(), przyjmuje wartosci po poprzedniej tablicy i ustawia nowy wariant
+    Board(const Board &other, const int x, const int y, const int variant) {
+        // alokujemy pamiec i przepisujemy wartosci z przekazanej tablicy "other"
+        field = new int *[DIM];
+        for (int i = 0; i < DIM; i++) {
+            field[i] = new int[DIM]();
+            for (int j = 0; j < DIM; j++) {
+                field[i][j] = other.field[i][j];
+            }
+        }
 
-void delete_board(int **board) {
-    for (int i = 0; i < DIMENSION; i++) {
-        delete[] board[i];
-    }
-    delete[] board;
-}
+        // value reprezentuje wartosc oczek nowo dodanej kostki
+        int value = 0;
 
+        // sprawdzamy 8 bitowa zmienna "variant", dla kazdego ustawionego bitu zerujemy odpowiediego sasiada i dodamy jego wartosc do zmiennej value
+        // przykladowo jezli zmienna variant == [0000 0101] to sasiad lewy i prawy zostanie pochloniety przez nowa kostke
+        // jezeli variant == [0000 0000] to poprostu wstawiamy kostke z wartoscia 1
+        if (variant == 0) {
+            value = 1;
+        }
+        if (variant & 1) {
+            // musimy tez sprawdzic czy sasiad istnieje, aby nie wyzerowac pola poza obrebem planszy
+            if (int tmp = get_square_left(x, y)) {
+                // jezeli variant obejmuje danego sasiada to dodajemy jego wartosc do zmiennej value
+                value += tmp;
+                // jego wartosc zostaje ustawiona na 0
+                set_square_left(x, y, 0);
+            }
+        }
+        if (variant & 2) {
+            if (int tmp = get_square_down(x, y)) {
+                value += tmp;
+                set_square_down(x, y, 0);
+            }
+        }
+        if (variant & 4) {
+            if (int tmp = get_square_right(x, y)) {
+                value += tmp;
+                set_square_right(x, y, 0);
+            }
+        }
+        if (variant & 8) {
+            if (int tmp = get_square_up(x, y)) {
+                value += tmp;
+                set_square_up(x, y, 0);
+            }
+        }
 
-int get_left_neighbour(int **board, const int x, const int y) {
-    return y == 0 ? 0 : board[x][y - 1];
-}
-
-int get_right_neighbour(int **board, const int x, const int y) {
-    return y == 2 ? 0 : board[x][y + 1];
-}
-
-int get_up_neighbour(int **board, const int x, const int y) {
-    return x == 0 ? 0 : board[x - 1][y];
-}
-
-int get_down_neighbour(int **board, const int x, const int y) {
-    return x == 2 ? 0 : board[x + 1][y];
-}
-
-void remove_left_neighbour(int **board, const int x, const int y) {
-    board[x][y - 1] = 0;
-}
-
-void remove_right_neighbour(int **board, const int x, const int y) {
-    board[x][y + 1] = 0;
-}
-
-void remove_up_neighbour(int **board, const int x, const int y) {
-    board[x - 1][y] = 0;
-}
-
-void remove_down_neighbour(int **board, const int x, const int y) {
-    board[x + 1][y] = 0;
-}
-
-std::vector<uint8_t> get_capture_possibilities(int **board, const int x, const int y) {
-    // up + right + down + left - 1111 <-> 15
-    // up + right + down        - 1110 <-> 14
-    // up + right + left        - 1101 <-> 13
-    // up + down + left         - 1011 <-> 11
-    // right + down + left      - 0111 <-> 7
-    // up + right               - 1100 <-> 12
-    // up + down                - 1010 <-> 10
-    // right + down             - 0110 <-> 6
-    // up + left                - 1001 <-> 9
-    // right + left             - 0101 <-> 5
-    // down + left              - 0011 <-> 3
-    // none                     - 0000 <-> 0
-
-    std::vector<uint8_t> possibilities = std::vector<uint8_t>();
-
-    const int left_neighbour = get_left_neighbour(board, x, y);
-    const int right_neighbour = get_right_neighbour(board, x, y);
-    const int up_neighbour = get_up_neighbour(board, x, y);
-    const int down_neighbour = get_down_neighbour(board, x, y);
-
-    if (up_neighbour + right_neighbour + down_neighbour + left_neighbour < 6 && up_neighbour + right_neighbour +
-        down_neighbour + left_neighbour > 4) {
-        possibilities.push_back(15);
-    }
-    if (up_neighbour + right_neighbour + down_neighbour < 6 && up_neighbour + right_neighbour + down_neighbour > 3) {
-        possibilities.push_back(14);
-    }
-    if (up_neighbour + right_neighbour + left_neighbour < 6 && up_neighbour + right_neighbour + left_neighbour > 3) {
-        possibilities.push_back(13);
-    }
-    if (up_neighbour + down_neighbour + left_neighbour < 6 && up_neighbour + down_neighbour + left_neighbour > 3) {
-        possibilities.push_back(11);
-    }
-    if (right_neighbour + down_neighbour + left_neighbour < 6 && right_neighbour + down_neighbour + left_neighbour >
-        3) {
-        possibilities.push_back(7);
-    }
-    if (up_neighbour + right_neighbour < 6 && up_neighbour + right_neighbour > 2) {
-        possibilities.push_back(12);
-    }
-    if (up_neighbour + down_neighbour < 6 && up_neighbour + down_neighbour > 2) {
-        possibilities.push_back(10);
-    }
-    if (right_neighbour + down_neighbour < 6 && right_neighbour + down_neighbour > 2) {
-        possibilities.push_back(6);
-    }
-    if (up_neighbour + left_neighbour < 6 && up_neighbour + left_neighbour > 2) {
-        possibilities.push_back(9);
-    }
-    if (right_neighbour + left_neighbour < 6 && right_neighbour + left_neighbour > 2) {
-        possibilities.push_back(5);
-    }
-    if (down_neighbour + left_neighbour < 6 && down_neighbour + left_neighbour > 2) {
-        possibilities.push_back(3);
-    }
-    if (possibilities.empty()) {
-        possibilities.push_back(0);
+        // ustawiamy wartosc nowo dodanej kostki na sume pozbieranych wartosci
+        set_square(x, y, value);
     }
 
-    return possibilities;
-}
-
-int **get_captured_board(int **board, const int x, const int y, const uint8_t variant) {
-    int **new_board = get_board(board);
-    int value = 0;
-
-    if (variant == 0) {
-        value = 1;
-    }
-    if (variant & 1) {
-        // 0001 -> left
-        value += get_left_neighbour(new_board, x, y);
-        remove_left_neighbour(new_board, x, y);
-    }
-    if (variant & 2) {
-        // 0010 -> down
-        value += get_down_neighbour(new_board, x, y);
-        remove_down_neighbour(new_board, x, y);
-    }
-    if (variant & 4) {
-        // 0100 -> right
-        value += get_right_neighbour(new_board, x, y);
-        remove_right_neighbour(new_board, x, y);
-    }
-    if (variant & 8) {
-        // 1000 -> up
-        value += get_up_neighbour(new_board, x, y);
-        remove_up_neighbour(new_board, x, y);
+    ~Board() {
+        for (int i = 0; i < DIM; i++) {
+            delete[] field[i];
+        }
+        delete[] field;
     }
 
-    new_board[x][y] = value;
+    bool square_empty(int x, int y) const { return field[x][y] == 0; }
 
-    return new_board;
-}
+    int get_square(int x, int y) const { return field[x][y]; }
+    int get_square_left(int x, int y) const { return y == 0 ? 0 : field[x][y - 1]; }
+    int get_square_right(int x, int y) const { return y == 2 ? 0 : field[x][y + 1]; }
+    int get_square_up(int x, int y) const { return x == 0 ? 0 : field[x - 1][y]; }
+    int get_square_down(int x, int y) const { return x == 2 ? 0 : field[x + 1][y]; }
 
-int simulate(int **board, const int max_depth, int current_depth, int variant_count) {
-    if (current_depth >= max_depth) { return variant_count; }
+    void set_square(int x, int y, int value) { field[x][y] = value; }
+    void set_square_left(int x, int y, int value) { field[x][y - 1] = value; }
+    void set_square_right(int x, int y, int value) { field[x][y + 1] = value; }
+    void set_square_up(int x, int y, int value) { field[x - 1][y] = value; }
+    void set_square_down(int x, int y, int value) { field[x + 1][y] = value; }
+
+    long long get_board_value() const {
+        long long num = 0;
+
+        for (int i = 0; i < DIM; i++) {
+            for (int j = 0; j < DIM; j++) {
+                num = num * 10 + field[i][j];
+            }
+        }
+
+        // debug
+        cerr << "zwracam wynik: " << num << endl << endl;
+        // debug
+
+        return num;
+    }
+
+    void print() const {
+        for (int i = 0; i < DIM; i++) {
+            for (int j = 0; j < DIM; j++) {
+                cerr << field[i][j] << " ";
+            }
+            cerr << endl;
+        }
+        cerr << endl;
+    }
+
+    // wyznacza wektor mozliwych przejec, jakie kostka wstawiona nadane pole jest w stanie wykonac
+    std::vector<int> get_capture_possibilities(const int x, const int y) const {
+        // kazda mozliwosc jest reprezentowana jako liczba 4 bitowa, kazdy bit reprezentuje jeden kierunek
+        // 1 jezeli kostka w danym kierunku moze zostac przejeta, 0 jezeli nie
+
+        std::vector<int> possibilities = std::vector<int>();
+
+        int u = get_square_up(x, y);
+        int r = get_square_right(x, y);
+        int d = get_square_down(x, y);
+        int l = get_square_left(x, y);
+
+        // zabezpiecznie przed zajmowaniem pol na ktorych nie ma kostek
+        if (u < 1) { u = 1000; }
+        if (r < 1) { r = 1000; }
+        if (d < 1) { d = 1000; }
+        if (l < 1) { l = 1000; }
+
+        // up + right + down + left - 1111 <-> 15
+        // up + right + down        - 1110 <-> 14
+        // up + right + left        - 1101 <-> 13
+        // up + down + left         - 1011 <-> 11
+        // right + down + left      - 0111 <-> 7
+        // up + right               - 1100 <-> 12
+        // up + down                - 1010 <-> 10
+        // right + down             - 0110 <-> 6
+        // up + left                - 1001 <-> 9
+        // right + left             - 0101 <-> 5
+        // down + left              - 0011 <-> 3
+        // none                     - 0000 <-> 0
+        if (u + r + d + l <= 6) { possibilities.push_back(15); }
+        if (u + r + d + 0 <= 6) { possibilities.push_back(14); }
+        if (u + r + 0 + l <= 6) { possibilities.push_back(13); }
+        if (u + 0 + d + l <= 6) { possibilities.push_back(11); }
+        if (0 + r + d + l <= 6) { possibilities.push_back(7); }
+        if (u + r + 0 + 0 <= 6) { possibilities.push_back(12); }
+        if (u + 0 + d + 0 <= 6) { possibilities.push_back(10); }
+        if (0 + r + d + 0 <= 6) { possibilities.push_back(6); }
+        if (u + 0 + 0 + l <= 6) { possibilities.push_back(9); }
+        if (0 + r + 0 + l <= 6) { possibilities.push_back(5); }
+        if (0 + 0 + d + l <= 6) { possibilities.push_back(3); }
+        if (possibilities.empty()) { possibilities.push_back(0); }
+
+        return possibilities;
+    }
+};
+
+long long simulate(const Board *board, const int max_depth, int current_depth) {
+    if (current_depth > max_depth) {
+        // debug
+        cerr << "zbyt gleboko" << endl;
+        // debug
+        return board->get_board_value();
+    }
     current_depth++;
 
-    for (int x = 0; x < DIMENSION; x++) {
-        for (int y = 0; y < DIMENSION; y++) {
-            if (board[x][y] == 0) {
-                variant_count++;
+    // debug
+    cerr << "poziom: " << current_depth - 1 << "/" << max_depth << endl;
+    board->print();
+    // debug
 
-                std::vector<uint8_t> possibilities = get_capture_possibilities(board, x, y);
+    long long board_values_sum = 0;
+    int free_squares = 0;
+
+    for (int x = 0; x < DIM; x++) {
+        for (int y = 0; y < DIM; y++) {
+            if (board->square_empty(x, y)) {
+                free_squares++;
+                std::vector<int> possibilities = board->get_capture_possibilities(x, y);
+
+                // debug
+                cerr << "nastepne rekurencje dla: [" << x << ", " << y << "] -> ";
+                for (int i = 0; i < possibilities.size(); i++) {
+                    cerr << bitset<4>(possibilities[i]) << ", ";
+                }
+                cerr << endl << endl;
+                // debug
 
                 for (int i = 0; i < possibilities.size(); i++) {
-                    int **new_board = get_captured_board(board, x, y, possibilities[i]);
-                    variant_count += simulate(new_board, max_depth, current_depth, variant_count) % (2 << 30);
-                    delete_board(new_board);
+                    cerr << "wykonuje: " << bitset<4>(possibilities[i]) << endl;
+                    Board *new_possible_board = new Board(*board, x, y, possibilities[i]);
+                    board_values_sum += simulate(new_possible_board, max_depth, current_depth);
+                    board_values_sum = board_values_sum % (2 << 30);
+                    delete new_possible_board;
                 }
             }
         }
     }
 
-    return variant_count;
+    if (free_squares == 0) {
+        // debug
+        cerr << "brak wolnych miejsc" << endl;
+        // debug
+        return board->get_board_value();
+    }
+
+    return board_values_sum;
 }
 
 int main() {
     int depth;
     cin >> depth;
     cin.ignore();
-    cerr << "depth: " << depth << endl;
 
-    int **board = get_board();
+    Board *board = new Board();
 
-    for (int i = 0; i < DIMENSION; i++) {
-        for (int j = 0; j < DIMENSION; j++) {
+    for (int i = 0; i < DIM; i++) {
+        for (int j = 0; j < DIM; j++) {
             int value;
             cin >> value;
             cin.ignore();
 
-            board[i][j] = value;
-
-            cerr << board[i][j] << " ";
+            board->set_square(i, j, value);
         }
-        cerr << endl;
     }
 
-    int result = simulate(board, depth, 0, 0);
-    delete_board(board);
-
+    int result = simulate(board, depth, 0);
     cout << result << endl;
+
+    delete board;
 }
